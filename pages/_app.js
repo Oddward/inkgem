e7e66b62e435dc89f61df411f16f8da6858ec962
Mugtaba G <lilmujster@hotmail.com>
2022-07-01 21:14:34 +0200

    initial commit; built starter blog template

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// You can only import global CSS files from here
+import '../styles/global.css'
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
